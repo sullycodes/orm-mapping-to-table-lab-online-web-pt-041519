c593a569042cf1b5f43e0f2c72c2ec583d27426e
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 13:59:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,18 +12,6 @@ class Student
     @grade = grade
   end
   
-  # def self.create_table
-  #   sql =  <<-SQL 
-  #     CREATE TABLE IF NOT EXISTS songs (
-  #       id INTEGER PRIMARY KEY, 
-  #       name TEXT, 
-  #       album TEXT
-  #       )
-  #       SQL
-  #   DB[:conn].execute(sql) 
-  # end
-  
-  
   def self.create_table 
     sql = <<-SQL 
             CREATE TABLE IF NOT EXISTS students (
@@ -34,5 +22,13 @@ class Student
           SQL
     DB[:conn].execute(sql)
   end
+
+  def drop_table 
+      sql = <<-SQL
+              DROP TABLE students
+            SQL
+      DB[:conn].execute(sql)
+  end
+
 
 end #Student
