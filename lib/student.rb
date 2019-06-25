8355179ce7b3bf4eb329a2cab26056119eba4aaa
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 14:05:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,16 +30,6 @@ class Student
       DB[:conn].execute(sql)
   end
 
-# def save
-#     sql = <<-SQL
-#       INSERT INTO songs (name, album) 
-#       VALUES (?, ?)
-#     SQL
- 
-#     DB[:conn].execute(sql, self.name, self.album)
- 
-#   end
-
 
   def save 
     sql = <<-SQL
