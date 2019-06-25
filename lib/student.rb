e9d64ad82930ca5e96f3c1b51de4d2da50ea232f
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 14:22:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,8 +41,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(name, grade) 
-    student = Student.new(name, grade) 
+  def self.create(hash) 
+    student = Student.new(hash) 
     student.save
     student
   end
