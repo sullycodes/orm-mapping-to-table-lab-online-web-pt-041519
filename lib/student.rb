f526fa66559d933f3b62bef16bd4bea30f6a5a83
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 13:55:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   end
   
   def self.create_table 
-    sql = <<-SQL CREATE TABLE students (
+    sql = <<-SQL CREATE TABLE IF NOT EXISTS students (
             id INTEGER PRIMARY KEY, 
             name TEXT, 
             grade INTEGER
