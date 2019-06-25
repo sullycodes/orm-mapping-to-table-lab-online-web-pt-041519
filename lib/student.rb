c291b2c30b0e3cd359e869370bd283455127a687
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 13:49:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,18 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+  
+  def self.create_table 
+    sql = <- SQL CREATE TABLE students (name, grade) VALUES (?,?) student.name, student.grade);
+    DB[:conn].execute(sql)
+  end
+
 end
