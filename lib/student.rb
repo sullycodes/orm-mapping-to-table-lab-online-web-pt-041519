460813e0dad5ed3f977dd4c6ed1bed94ca04f119
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 14:03:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,10 +42,11 @@ class Student
 
 
   def save 
-    sql = <<-SQL 
-            INSERT INTO students (name, grade)
-            VALUES (?,?)
+    sql = <<-SQL
+          INSERT INTO students (name,grade)
+          VALUES (?,?)
           SQL
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
 end #Student
