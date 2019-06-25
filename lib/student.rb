488e4ebd9100e0e2b3c7b9b9574444b09cb1b6e7
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 13:54:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,10 +14,10 @@ class Student
   
   def self.create_table 
     sql = <<-SQL CREATE TABLE students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade INTEGER
-      )
+            id INTEGER PRIMARY KEY, 
+            name TEXT, 
+            grade INTEGER
+            )
       SQL
     DB[:conn].execute(sql)
   end
